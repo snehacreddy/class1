a424a74a0e91f7f50f1ec11a6c5511f9bb0a769c
snehacreddy <snehacgbd@gmail.com>
2019-10-13 03:15:13 -0700

    message

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("i am new user");
+
+console.error("occured error");
+console.warn("there is a warning");
+alert("i am alart");
+var name = "sneha"
+
+   
+
+
+   function myFunction(elem){
+     console.log(elem);
+     elem.style.color="green"
+ }
+ function funFam(par){
+     par.innerHTML = "s"
+ }
